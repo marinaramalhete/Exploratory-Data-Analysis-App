e78c56d4ef423a9db11c12ec7b5746ad96854e05
Marina Ramalhete <marinaramalhete@outlook.com>
2020-04-25 13:17:57 -0300

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+  
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"souzamarina92@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
